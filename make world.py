458cf335907c69c002fe621811024a643bb37235
realeed <makonygina@yandex.ru>
2024-01-13 20:37:18 +0300

    экран

--- make world.py
+++ make world.py
@@ -2,17 +2,78 @@ import pygame
 
 pygame.init()
 
-size = 630, 500
-screen = pygame.display.set_mode(size)
+clock = pygame.image.Clock()
+
+
+width, height = 650, 650
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Magical Explore')
 
-bg_image = pygame.image.load('img/forest.png')
+#фон
+sky_image = pygame.image.load('img/8Sky.png')
+hills_image = pygame.image.load('img/7Hills.png')
+forest_image = pygame.image.load('img/6Forest.png')
+bushes_image = pygame.image.load('img/5BackBushes.png')
+ground_image = pygame.image.load('img/4Ground.png')
 
+tile_size = 65
+
+#рисуем клеточное поле
+def draw_board():
+    for a in range(0, 10):
+        pygame.draw.line(screen, (255, 255, 255), (0, a * tile_size), (width, a * tile_size))
+        pygame.draw.line(screen, (255, 255, 255), (a * tile_size, 0), (a * tile_size, height))
+
+
+world_data = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+
+
+class World():
+    def __init__(self, data):
+        self.tile_list = []
+
+        grass_image = pygame.image.load('img/grass.png')
+
+        r_k = 0
+        for row in data:
+            c_k = 0
+            for tile in row:
+                if tile == 1:
+                    background = pygame.transform.scale(grass_image, (tile_size, tile_size))
+                    background_rect = background.get_rect()
+                    background_rect.x = c_k * tile_size
+                    background_rect.y = r_k * tile_size
+                    tile = (background, background_rect)
+                    self.tile_list.append(tile)
+                c_k += 1
+            r_k += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+
+
+world = World(world_data)
 run = True
-
 while run:
-    screen.blit(bg_image, (0, 0))
-
+    screen.blit(sky_image, (0, 0))
+    screen.blit(hills_image, (0, 0))
+    screen.blit(forest_image, (0, 0))
+    screen.blit(bushes_image, (0, 0))
+    screen.blit(ground_image, (0, 0))
+    draw_board()
+    world.draw()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
